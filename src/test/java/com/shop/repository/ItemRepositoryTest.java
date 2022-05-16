0b09ef42366e148c6c105ac89cfa63fa71d2608b
2garam <2garamssi@gmail.com>
2022-05-17 00:16:28 +0900

    Jpa simple basic test

---
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -0,0 +1,108 @@
+package com.shop.repository;
+
+import com.shop.constant.ItemSellStatus;
+import com.shop.entity.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+class ItemRepositoryTest {
+
+        @Autowired
+        ItemRepository itemRepository;
+
+        @Test
+        @DisplayName("상품 저장 테스트")
+        public void createItemTest(){
+                Item item = new Item();
+                item.setItemNm("테스트 상품");
+                item.setPrice(10000);
+                item.setItemDetail("테스트 상품 상세 설명");
+                item.setItemSellStatus(ItemSellStatus.SELL);
+                item.setStockNumber(100);
+                item.setRegTime(LocalDateTime.now());
+                item.setUpdateTime(LocalDateTime.now());
+                Item saveItem = itemRepository.save(item);
+                System.out.println(saveItem.toString());
+
+
+        }
+
+        public void createItemList() {
+                for(int i=0; i<10; i++) {
+                        Item item = new Item();
+                        item.setItemNm("테스트 상품" + i);
+                        item.setPrice(10000 + i);
+                        item.setItemDetail("테스트 상품 상세 설명" + i);
+                        item.setItemSellStatus(ItemSellStatus.SELL);
+                        item.setStockNumber(100); item.setRegTime(LocalDateTime.now());
+                        item.setUpdateTime(LocalDateTime.now());
+                        Item saveItem = itemRepository.save(item);
+                }
+        }
+
+        /**
+         * 쿼리 메소드를 이용할 때 가장 많이 사용하는 문법으로 find를 사용한다
+         * 엔티티 이름은 생략이 가능하며 By뒤에는 사용할 변수의 이름을 적는다
+         *
+         * find + ( 엔티티 이름 ) + By + 변수이름
+         * */
+
+        @Test
+        @DisplayName("상품명 조회 테스트")
+        public void findByitemNmTest() {
+                this.createItemList();
+                List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
+                for (Item item : itemList) {
+                        System.out.println(item.toString());
+                }
+        }
+
+        @Test
+        @DisplayName("상품명, 상품상세설명 or 테스트")
+        public void findByItemNmOrItemDetailTest() {
+                this.createItemList();
+                List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
+                for(Item item: itemList) {
+                        System.out.println(item.toString());
+                }
+        }
+
+        @Test
+        @DisplayName("가격 LessThan")
+        public void findByPriceLessThanTest() {
+                this.createItemTest();
+                List<Item> itemList = itemRepository.findByPriceLessThan(10005);
+                for(Item item: itemList) {
+                        System.out.println(item.toString());
+                }
+        }
+
+
+        /**
+         * OrderBy + 속성명 + [Desc | Asc]
+         * */
+
+        @Test
+        @DisplayName("가격 내림차순 조회 테스트")
+        public void findByPriceLessThanOrderByPriceDescTest() {
+                this.createItemTest();
+                List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
+                for(Item item: itemList) {
+                        System.out.println(item.toString());
+                }
+        }
+
+
+
+
+}
